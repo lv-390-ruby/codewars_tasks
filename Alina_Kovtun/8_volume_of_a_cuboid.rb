cdbdc0e591c2f4537e442e97e7fe28dfb089ca4b
Alina Kovtun <alinakovtun11@gmail.com>
2019-03-06 23:14:44 +0200

    Add task '8-volume-of-a-cuboid'

---
+++ Alina_Kovtun/8_volume_of_a_cuboid.rb
@@ -0,0 +1,7 @@
+def get_volume_of_cuboid(length, width, height)
+  length * width * height
+end
+
+print get_volume_of_cuboid(2, 3, 4)   # 24
+print get_volume_of_cuboid(4, 6, 8)   # 192
+print get_volume_of_cuboid(7, 14, 26) # 2548
