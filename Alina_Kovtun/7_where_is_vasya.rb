ffc1fe68ac7378566be07e60e7355ecc5d889fe5
Alina Kovtun <alinakovtun11@gmail.com>
2019-03-09 19:41:07 +0200

    Add docstring, removed print

--- Alina_Kovtun/7_where_is_vasya.rb
+++ Alina_Kovtun/7_where_is_vasya.rb
@@ -1,3 +1,13 @@
+# Vasya stands in line with number of people p (including Vasya),
+# but he doesn't know exactly which position he occupies.
+# Find the number of different positions Vasya can occupy.
+
+# Input
+# As an input you have 3 numbers:
+# 1. p - Total amount of people in the line;
+# 2. bef - Number of people standing in front of him
+# 3. aft - Number of people standing behind him
+
 def where_is_he(p, bef, aft)
   if (p - bef - aft > 1)
     aft + 1
@@ -5,6 +15,3 @@ def where_is_he(p, bef, aft)
     p - bef
   end
 end
-
-print where_is_he(3, 1, 1)  # 2
-print where_is_he(5, 2, 3)  # 3
