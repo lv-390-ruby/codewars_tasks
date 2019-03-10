1d51071b66eccd81a19351be662c0b7ca2572bb9
Alina Kovtun <alinakovtun11@gmail.com>
2019-03-10 14:05:48 +0200

    Add docstring, remove print

--- Alina_Kovtun/8_mpg_to_kmpl.rb
+++ Alina_Kovtun/8_mpg_to_kmpl.rb
@@ -1,3 +1,8 @@
+# Get the number of kilometers per liter based on the number of miles per imperial gallon.
+
+# Input: mpg - miles per imperial gallon
+# Output: kmpl - kilometers per liter (rounded off to two decimal points)
+
 IMPERIAL_GALLON_TO_LITRE = 4.54609188
 MILE_TO_KILOMETER = 1.609344
 
@@ -5,7 +10,3 @@ def converter(mpg)
   kmpl = mpg / IMPERIAL_GALLON_TO_LITRE * MILE_TO_KILOMETER
   kmpl.round(2)
 end
-
-puts converter(12)  #4.25
-puts converter(24)  #8.5
-puts converter(36)  #12.74
