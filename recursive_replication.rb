331cab671b472778a82ea9ac4a993dfe5e584ebc
kmandziy <kmandzii@gmail.com>
2019-03-11 09:54:04 +0200

    Add docstring to recursive_replication.rb

--- recursive_replication.rb
+++ recursive_replication.rb
@@ -1,3 +1,11 @@
+#The function should return an array containing repetitions of the number argument. 
+
+#If the times argument is negative, return an empty array.
+
+#replicate(3, 5) should return [5,5,5]
+
 def replicate(times, number)
   times > 0 ? arr = [number] + replicate(times-1,number) : []
 end
+
+
