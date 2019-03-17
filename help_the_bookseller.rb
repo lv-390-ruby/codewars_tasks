934db37f4c47fd4bb8f471e99acf05e6a473d5fc
unseensquirrel <webkng@gmail.com>
2019-03-17 12:59:57 +0200

    added final solution

--- help_the_bookseller.rb
+++ help_the_bookseller.rb
@@ -1,11 +1,8 @@
 def stockList(listOfArt, listOfCat)
-  if listOfArt.length == 0 || listOfArt.length == 0
-    return ""
-  end
+  return "" if listOfArt.empty? or listOfCat.empty?
 
-  listOfArt = listOfArt.select {|elem| elem.match(/^#{listOfCat}/) }
-#   listOfArt.select! {|elem| elem =~ /\A[ABC]/}
   listOfArt = Hash[listOfArt.map {|el| el.split ' '}]
+  listOfArt.select! {|key, val| listOfCat.join.include? key[0] }
   listOfArt = Hash[listOfArt.values.map(&:to_i).zip(listOfArt.keys)].invert
   answer = Hash[listOfCat.product([0])]
   listOfArt.select do |key, val|
@@ -13,5 +10,5 @@ def stockList(listOfArt, listOfCat)
       answer[key[0]] += val
     end
   end
-  answer = answer.map{|k, v| "#{k} : #{v}"}.join(') - (').prepend('(') << ")"
+  answer = answer.map{|k,v| "#{k} : #{v}"}.join(') - (').prepend('(') << ")"
 end
