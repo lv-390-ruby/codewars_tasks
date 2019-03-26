4bb21a6ac3be09ac869537d485b0cb0f5c515f3d
unseensquirrel <webkng@gmail.com>
2019-03-26 21:33:02 +0200

    Deleted return

--- positives_count.rb
+++ positives_count.rb
@@ -3,6 +3,6 @@
 # If the input array is empty or null, return an empty array.
 
 def count_positives_sum_negatives(lst)
-  return ((lst.empty?) || (lst == nil)) ? []: [(lst.reject {|x| x < 1}.count),(lst.reject {|x| x > -1}).sum]
+  ((lst.empty?) || (lst == nil)) ? []: [(lst.reject {|x| x < 1}.count),(lst.reject {|x| x > -1}).sum]
 end
 
