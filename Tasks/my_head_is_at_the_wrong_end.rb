9de9b1743d9a97e4c6e7dd6cf5f7ec7c1110be24
kmandziy <kmandzii@gmail.com>
2019-03-28 18:33:48 +0200

    Refactoring my_head_is_at_the_wrong_end.rb

--- Tasks/my_head_is_at_the_wrong_end.rb
+++ Tasks/my_head_is_at_the_wrong_end.rb
@@ -1,8 +1,9 @@
-#The function re-arrange the array so that the animal is the right way round (head, body, tail).
+# The function re-arrange the array so that the animal
+# is the right way round (head, body, tail).
 
-#fix_the_meerkat(arr = ["tail", "body", "head"]) should return ["head", "body", "tail"]
+# fix_the_meerkat(arr = ["tail", "body", "head"])
+# returns ["head", "body", "tail"]
 
 def fix_the_meerkat(arr)
   arr.reverse
 end
-
