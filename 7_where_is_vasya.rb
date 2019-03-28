87b3cd66335fe0bbc3bfa5a4247c5b485410170e
Alina Kovtun <alinakovtun11@gmail.com>
2019-03-28 13:00:40 +0200

    refactoring by rubocop

--- 7_where_is_vasya.rb
+++ 7_where_is_vasya.rb
@@ -1,17 +1,18 @@
+# Author: Alina Kovtun
+#
 # Vasya stands in line with number of people p (including Vasya),
 # but he doesn't know exactly which position he occupies.
 # Find the number of different positions Vasya can occupy.
-
 # Input
 # As an input you have 3 numbers:
-# 1. p - Total amount of people in the line;
+# 1. num - Number of people in the line;
 # 2. bef - Number of people standing in front of him
 # 3. aft - Number of people standing behind him
 
-def where_is_he(p, bef, aft)
-  if (p - bef - aft > 1)
+def where_is_he(num, bef, aft)
+  if num - bef - aft > 1
     aft + 1
   else
-    p - bef
+    num - bef
   end
 end
