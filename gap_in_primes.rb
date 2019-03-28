a5686af247c600ff6316ed7de5f9f1b66270447c
Alina Kovtun <alinakovtun11@gmail.com>
2019-03-28 12:00:46 +0200

    add task

---
+++ gap_in_primes.rb
@@ -0,0 +1,32 @@
+# Author: Alina Kovtun
+#
+# A prime gap of length 'last' is a run of last-1 consecutive composite numbers
+# between two successive primes
+#
+# parameters:
+# gap (integer >= 2) which indicates the gap we are looking for
+# first (integer > 2) which gives the start of the search (first inclusive)
+# last (integer >= first) which gives the end of the search (last inclusive)
+#
+# The function should return the first pair of two prime numbers spaced
+# with a gap of gap between the limits first, last if these numbers exist otherwise nil
+
+def gap(gap, first, last)
+  (first..last).each do |num|
+    if prime?(num) && prime?(num + gap) && no_primes?(num, num + gap)
+      return [num, num + gap]
+    end
+
+    num += gap
+  end
+
+  nil
+end
+
+def prime?(num)
+  (2..Math.sqrt(num.to_f)).none? { |i| (num % i).zero? }
+end
+
+def no_primes?(first, last)
+  ((first + 1)...last).none? { |num| prime?(num) }
+end
