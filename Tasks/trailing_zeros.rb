ac5b9fa03ce48f05044c5640e25fc20da750beca
kmandziy <kmandzii@gmail.com>
2019-03-28 23:53:10 +0200

    Code refactoring
    - removing unnecessary space
    - change name of parameter in function

--- Tasks/trailing_zeros.rb
+++ Tasks/trailing_zeros.rb
@@ -1,9 +1,8 @@
-# Calculate the number of trailing zeros in a factorial of a given number.
-
+# Calculate the number of trailing zeros in a factorial of a given number
 # 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
 
-def zeros(n)
-  n < 5 ? 0 : (n / 5) + zeros(n / 5)
+def zeros(number)
+  number < 5 ? 0 : (number / 5) + zeros(number / 5)
 end
 
 def run_task
