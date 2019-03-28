33b115cd47e2a13bce06e11351c151a6cb3f083b
Alina Kovtun <alinakovtun11@gmail.com>
2019-03-29 00:27:13 +0200

    Add run_task

--- 8_volume_of_a_cuboid.rb
+++ 8_volume_of_a_cuboid.rb
@@ -6,3 +6,16 @@
 def get_volume_of_cuboid(length, width, height)
   length * width * height
 end
+
+def run_task
+  print "Length: "
+  length = gets.chomp.to_i
+  print "Width: "
+  width = gets.chomp.to_i
+  print "Height: "
+  height = gets.chomp.to_i
+
+  puts "The volume of cuboid is #{get_volume_of_cuboid(length, width, height)}"
+end
+
+run_task
