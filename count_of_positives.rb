5cfb62f031f715215d899ace2b7568668365b6b5
unseensquirrel <webkng@gmail.com>
2019-03-29 01:27:57 +0200

    added run task

--- count_of_positives.rb
+++ count_of_positives.rb
@@ -4,9 +4,12 @@
 # positives numbers and the second element is sum of negative numbers.
 # If the input array is empty or null, return an empty array.
 def count_positives_sum_negatives(lst)
-  lst.nil? || lst.empty? ? [] : [lst.reject { |x| x < 1 }.count, (lst.reject { |x| x > -1 }).sum]
+  return [] if lst.nil? || lst.empty?
+
+  [lst.reject { |x| x < 1 }.count, (lst.reject { |x| x > -1 }).sum]
 end
 
 def run_task
-
+  print 'Enter array of numbers: (without [], only numbers)'
+  lst = gets.chomp.split(',').map { |s| s.to_i }
 end
