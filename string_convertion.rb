890bf60fbe41ba3f52b6a36b622d857e048850d5
unseensquirrel <webkng@gmail.com>
2019-03-29 01:44:30 +0200

    Added run_tasks

--- string_convertion.rb
+++ string_convertion.rb
@@ -1,9 +1,11 @@
 # Author: Natalia Kozoriz
 # Here our task is to convert string to number
-def string_to_number(s)
-  s.to_i
+def string_to_number(string)
+  string.to_i
 end
 
 def run_task
-
+  print 'Enter your string: '
+  string = gets.chomp
+  puts "Your new number is: #{string_to_number(string)}"
 end
