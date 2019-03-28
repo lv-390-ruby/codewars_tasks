18dcfd14aa58cfba6dd8314be88d51487b60a108
kmandziy <kmandzii@gmail.com>
2019-03-28 19:52:58 +0200

    Update recursive_replication.rb

--- recursive_replication.rb
+++ recursive_replication.rb
@@ -1,9 +1,18 @@
-#The function should return an array containing repetitions of the number argument. 
+# The function should return an array containing
+# repetitions of the number argument
 
-#If the times argument is negative, return an empty array.
+# If the times argument is negative, return an empty array.
 
-#replicate(3, 5) should return [5,5,5]
+# replicate(3, 5) should return [5,5,5]
 
 def replicate(times, number)
-  times > 0 ? arr = [number] + replicate(times-1,number) : []
+  times > 0 ? [number] + replicate(times - 1, number) : []
+end
+
+def run_task
+  puts 'Enter the size of array'
+  times = gets.to_i
+  puts 'Enter the number that must be repeated'
+  number = gets.to_i
+  puts replicate(times, number).to_s
 end
