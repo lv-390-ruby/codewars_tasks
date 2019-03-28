8479ff884d7fa1e3d0d4f8124f790ab9cba7c932
kmandziy <kmandzii@gmail.com>
2019-03-28 20:25:53 +0200

    Add run_task to file - Change square_or_square_root.rb destination - Add run_task to square-or-square_root.rb - Fix run_task in nba_cup.rb

---
+++ Tasks/square_or_square_root.rb
@@ -0,0 +1,18 @@
+# The function takes square root if the number 
+# has an integer square root, otherwise square the number
+# square_or_square_root ([4,3,9,7,2,1]) 
+
+#-> [2,9,3,49,4,1]
+
+def square_or_square_root(arr)
+  arr.map do |num|
+    sqrt = num**0.5
+    (sqrt % 1).zero? ? num**0.5 : (num**2).to_i
+  end
+end
+
+def run_task
+  puts 'Enter numbers'
+  arr = gets.chomp.split(' ').map(&:to_i)
+  puts square_or_square_root(arr).to_s
+end
