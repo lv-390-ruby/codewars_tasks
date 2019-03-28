542a5e70959f69df8a9c267a38355a3fca9a9510
unseensquirrel <webkng@gmail.com>
2019-03-28 15:14:16 +0200

    Added Author

--- count_of_positives.rb
+++ count_of_positives.rb
@@ -1,3 +1,4 @@
+# Author : Natalia Kozoriz
 # Given an array of integers.
 # Return an array, where the first element is the count of
 # positives numbers and the second element is sum of negative numbers.
