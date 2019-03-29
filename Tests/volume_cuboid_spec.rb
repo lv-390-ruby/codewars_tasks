abdc54a6c5b194b88b8df56902d0cb1d505c819f
kmandziy <kmandzii@gmail.com>
2019-03-29 10:12:55 +0200

    Update volume_cuboid_spec.rb

--- Tests/volume_cuboid_spec.rb
+++ Tests/volume_cuboid_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../Tasks/volume_of_a_cuboid'
 
-RSpec.describe 'get_volume_of_cuboid' do
+RSpec.describe '#get_volume_of_cuboid' do
   context 'when testing the method get_volume_of_cuboid' do
     it 'calculates the volume of cuboid' do
       volume = get_volume_of_cuboid(1, 2, 3)
