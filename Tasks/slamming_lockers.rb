aedf38213ac61334a5b9dd81d2732438abac027e
unseensquirrel <webkng@gmail.com>
2019-03-29 12:36:56 +0200

    Moves files to tasks folder

--- Tasks/slamming_lockers.rb
+++ Tasks/slamming_lockers.rb
@@ -1,8 +1,15 @@
+# Author: Natalia Kozoriz
 # The goal of this kata is to determine which lockers are open at the end of Johnny's running.
-# The program accepts an integer giving the total number of lockers, and should output an array filled with the locker numbers of 
+# The program accepts an integer giving the total number of lockers, and should output an array filled with the locker numbers of
 # those which are open at the end of his run.
 
 def locker_run x
-  (1..sqrt(x)).map { |i| i * i}
+  (1..Math.sqrt(x)).map { |i| i * i}
 end
 
+def run_task
+  print 'Enter total number of lockers:'
+  x = gets.chomp.to_i
+  puts "Here is an array filled with the locker numbers of
+  those which are open at the end of his run : #{locker_run(x)}"
+end
