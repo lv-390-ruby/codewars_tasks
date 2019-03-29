1a8f7ae43ff5db91b0c2ebf5f84ccdcde275b8fe
Alina Kovtun <alinakovtun11@gmail.com>
2019-03-29 03:02:31 +0200

    moved files to folder Tasks

--- Tasks/volume_of_a_cuboid.rb
+++ Tasks/volume_of_a_cuboid.rb
@@ -1,8 +1,19 @@
 # Author: Alina Kovtun
-# 
+#
 # Calculate the volume of a cuboid.
 # Input: length, width and the height of the cuboid.
 
 def get_volume_of_cuboid(length, width, height)
   length * width * height
+end
+
+def run_task
+  print "Length: "
+  length = gets.chomp.to_i
+  print "Width: "
+  width = gets.chomp.to_i
+  print "Height: "
+  height = gets.chomp.to_i
+
+  puts "The volume of cuboid is #{get_volume_of_cuboid(length, width, height)}"
 end
