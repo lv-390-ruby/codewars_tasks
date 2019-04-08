4ec4282e547373bfb3e027e8f5ade82191e2d588
AbdulRustam <abdulrustampi33@gmail.com>
2019-04-08 23:36:22 +0300

    Fix non corect change

--- Tasks/cube.rb
+++ Tasks/cube.rb
@@ -2,14 +2,14 @@
 # The function found n and take arg m
 # if such a n exists or -1 if there is no such n.
 
-def find_nb(m)
-  n = 1
+def find_nb(sum_fun)
+  count = 1
   sum = 1
-  while sum < m
-   n += 1
-   sum += n**3
+  while sum < sum_fun
+   count += 1
+   sum += count**3
   end
-return sum == m ? n : -1
+  sum == sum_fun ? count : -1
 end
 
 def run_task
