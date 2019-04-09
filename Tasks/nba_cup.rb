f29a5912369f1d57763cfca59fe7956ab830fac6
kmandziy <kmandzii@gmail.com>
2019-04-09 11:43:21 +0300

    Code refactoring

--- Tasks/nba_cup.rb
+++ Tasks/nba_cup.rb
@@ -7,9 +7,9 @@
 # 'Indiana Pacers 103 Memphis Grizzlies 112,'\
 # 'Los Angeles Lakers 111 Minnesota Timberwolves 112,'\
 # 'Phoenix Suns 95 Dallas Mavericks 111,'\
-# 'Portland Trail Blazers 112 New Orleans Pelicans 94'
-
-# # # Given a team (parameter : to_find) e.g:
+# 'Portland Trail Blazers 112 New Orleans Pelicans 94,'\
+# 'New York Knicks 101 Atlanta Hawks 112.3'
+# # # # Given a team (parameter : to_find) e.g:
 
 # to_find = 'New York Knicks'
 
@@ -22,78 +22,84 @@
 # If a score is a float number will
 
 # returns "Error(float number):the concerned string"
-# If the team name "" returns ""
-
-def error_float(game, to_find, enemy_team, str)
-  if game[to_find].include?('.')
-    "Error(float number):#{to_find} #{game[to_find]} " \
-    "#{enemy_team} #{game[enemy_team]}"
-  elsif game[enemy_team].include?('.')
-    "Error(float number):#{enemy_team} #{game[enemy_team]} " \
-    "#{to_find} #{game[to_find]}"
-  else
-    str
+class Nba
+  def initialize(result_sheet, to_find)
+    @games = get_games(result_sheet)
+    @to_find = to_find
+    @params = { W: 0, D: 0, L: 0, Scored: 0, Conceded: 0, Points: 0 }
+    @rank = ''
   end
-end
 
-def comparison(res, to_find_score, enemy_score)
-  res[:w] += 1 if to_find_score > enemy_score
-  res[:l] += 1 if to_find_score < enemy_score
-  res[:d] += 1 if to_find_score == enemy_score
-  res[:points] = res[:w] * 3 + res[:d]
-  res.inspect
-end
-
-def score_comparison(res, to_find_score, enemy_score, to_find, str)
-  comparison(res, to_find_score, enemy_score)
-  res[:conceded] += enemy_score
-  res[:scored] += to_find_score
-  unless str.include?('Error')
-    str = "#{to_find}:W=#{res[:w]};D=#{res[:d]};L=#{res[:l]};"\
-    "Scored=#{res[:scored]};Conceded=#{res[:conceded]};"\
-    "Points=#{res[:points]}"
+  def get_games(result_sheet)
+    games = result_sheet.split(',')
+    games.map! do |game|
+      Hash[game.scan(/(?<team>.+?)(?<score>\d+\.+\d{1,3}|\d{1,3})/)]
+    end
   end
-  str
-end
 
-def parse_result_sheet(game)
-  teams = game.split(/(?!\d{1,3}[a-z]{1,3})(?:\d+\.+\d{1,3}|\d{1,3})/)
-  score = game.scan(/(?!\d{1,3}[a-z]{1,3})(?:\d+\.+\d{1,3}|\d{1,3})/)
-  teams.map!(&:strip)
-  @game = teams.zip(score).to_h
-end
+  def ranking_nba
+    @games.each do |game|
+      game.each do |key, value|
+        next if checked_team_non_existance(key)
+        break if find_error_float(key, value)
 
-def find_score(game, to_find)
-  @enemy_team = game.keys[0] == to_find ? game.keys[1] : game.keys[0]
-  @enemy_score = game[@enemy_team].to_i
-  @to_find_score = game[to_find].to_i
-end
-
-def check_team(team_exists, str, to_find)
-  team_exists ? str : "#{to_find}:This team didn't play!"
-end
-
-def nba_cup(result_sheet, to_find)
-  results = { w: 0, d: 0, l: 0, scored: 0, conceded: 0, points: 0 }
-  team_exists = false
-  str = ''
-  return str if to_find.empty?
-  result_sheet.split(',').each do |game|
-    parse_result_sheet(game)
-    find_score(@game, to_find)
-    next unless @game.key?(to_find)
-    str = error_float(@game, to_find, @enemy_team, str)
-    str = score_comparison(results, @to_find_score, @enemy_score, to_find, str)
-    team_exists = true
+        find_enemy(game)
+        compare_game_score(@params, @to_find_score, @enemy_score)
+        show_string_rank
+      end
+    end
+    @rank
   end
-  check_team(team_exists, str, to_find)
+
+  def checked_team_non_existance(key)
+    if key.strip != @to_find
+      return true if @rank.include?(@to_find.to_s)
+
+      @rank = "#{@to_find}: This team didn't play!"
+    end
+  end
+
+  def find_enemy(game)
+    game.each do |key, value|
+      if key.strip != @to_find
+        @enemy_team = key.strip
+        @enemy_score = value.to_i
+      else
+        @to_find_score = value.to_i
+      end
+    end
+  end
+
+  def show_string_rank
+    @rank = "#{@to_find}:"
+    @params.each do |key, value|
+      @rank << "#{key}=#{value};"
+    end
+  end
+
+  def compare_game_score(params, to_find_score, enemy_score)
+    params[:W] += 1 if to_find_score > enemy_score
+    params[:D] += 1 if to_find_score == enemy_score
+    params[:L] += 1 if to_find_score < enemy_score
+    params[:Scored] += to_find_score
+    params[:Conceded] += enemy_score
+    params[:Points] = params[:W] * 3 + params[:D]
+    params
+  end
+
+  def find_error_float(key, value)
+    @rank = "Error float:#{key}#{value}" if value.include?('.')
+  end
 end
 
 def run_task
-  puts 'Enter string like this'\
-  '"Lviv 201 Odessa 120, Odessa 102 Berlin 292"'
+  puts 'Please enter the string like this'\
+  '"Odessa 289 Lviv 293, Kiev 292 Lviv 231"'
   result_sheet = gets.chomp
-  puts 'Enter name of team'
+  puts 'Enter team name'
   to_find = gets.chomp
-  puts nba_cup(result_sheet, to_find)
+  nba = Nba.new(result_sheet, to_find)
+  puts nba.ranking_nba
 end
+
+run_task
